6594e3142e51ad15ca6531e4e3f492b99f84c570
Your Name <baba_mmx@yahoo.it>
2012-02-21 11:12:15 +0100

    __reset_drupalrel mods

--- __reset_drupalrel.php
+++ __reset_drupalrel.php
@@ -15,12 +15,14 @@
 	$query_string = htmlentities(urlencode($itql));
 
 	$url = variable_get('fedora_repository_url', 'http://localhost:8080/fedora/risearch');
-echo "$url\n\n";
+	echo "Fedora Repository URL: $url\n";
+	echo "NB: each dot will be an object excluded from the deletion of the Drupal Rel\n\n";
+
   	$url.= "?type=tuples&flush=TRUE&format=Sparql&limit=&offset=0&lang=itql&stream=on&query=" . $query_string;
   	$content = do_curl($url);
   
 	if (empty($content)) {
-		echo "content is empty";
+		echo "content is empty... check Fedora Repository connection.\n\n";
 		exit (0);
 	}
 	
@@ -28,24 +30,20 @@ echo "$url\n\n";
 
 	if ( count( $items->results->result ) > 0 ) {
 		foreach ( $items->results->result as $res ) {
-			$objects[] =  (array) $res;
+			$object = (array) $res;
+
+			$pid = $object['identifier'];
+			$fedora_item = new Fedora_Item($pid);
+			$datastreams = $fedora_item->get_datastreams_list_as_array();
+
+			if (isset($datastreams[$DS_ID])) {
+				echo " [ purging DS Drupal Rel of object: $pid ]\n";
+				$fedora_item->purge_datastream($DS_ID);
+			}
+			else {
+				echo ".";
+			}
+
 		}
 	}
-
-	foreach ($objects as $object) {
-		$pid = $object['identifier'];
-		$fedora_item = new Fedora_Item($pid);
-		$datastreams = $fedora_item->get_datastreams_list_as_array();
-
-		if (isset($datastreams[$DS_ID])) {
-			echo " [ purging DS Drupal Rel of object: $pid ]\n";
-			$fedora_item->purge_datastream($DS_ID);
-		}
-		else {
-			echo ".";
-		}
-
-		//jhdskvnh
-	}
-
 ?>
